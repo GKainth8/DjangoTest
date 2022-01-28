96e4fd38a8527cfd53e649f957a88700a7ec3b62
Sadarax <gagankainth@hotmail.com>
2022-01-28 22:46:28 +0000

    Adding hahah

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -14,7 +14,7 @@ def home(request):
 def index(request):
     return HttpResponse("Hello World")
 
-#helloworld
+#whattheactualfuck
 def book_by_id(request, book_id):
     book = Book.objects.get(pk=book_id)
     return render(request, 'book_details.html', {'book':book})
