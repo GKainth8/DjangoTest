e96f01167b7b29bd52cf680e34e5fad92bcfadc0
Sadarax <gagankainth@hotmail.com>
2022-01-28 22:37:46 +0000

    Adding hahah

---
+++ mysite/myapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+from django.http import HttpResponse
+
+from .models import Book
+
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Main Page")
+
+def index(request):
+    return HttpResponse("Hello World")
+
+hahah
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'book_details.html', {'book':book})
+
+
